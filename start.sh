1ecad4bf96a42de8ff23b47108de97bd55a7caea
SebM <seb.morgant@gmail.com>
2024-11-17 17:22:51 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Start Supervisor to manage services
-exec /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+# Check if TARGET_URL is set, exit if not
+if [ -z "$TARGET_URL" ]; then
+  echo "Error: TARGET_URL environment variable is required."
+  exit 1
+fi
+
+# Start the launch_chromium.sh script in the background
+./launch_chromium.sh &
+
+# Start the Python API server
+python3 -u chromium_api.py
