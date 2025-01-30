5749da903050793a952e7d3049f2f986a6d3ceee
SebM <seb.morgant@gmail.com>
2025-01-30 00:40:32 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,5 +9,18 @@ fi
 # Start the launch_chromium.sh script in the background
 ./launch_chromium.sh &
 
-# Start the Python API server
-python3 -u chromium_api.py
+# Get Chromium process ID (so we can track it)
+CHROMIUM_PID=$!
+
+# Start the Python API server in the background
+python3 -u chromium_api.py &
+API_PID=$!
+
+# Wait for Chromium to exit
+wait $CHROMIUM_PID
+
+echo "Chromium has exited. Stopping the API server..."
+kill $API_PID  # Stop Flask API
+
+echo "Exiting container..."
+exit 0  # This will allow the container to stop
